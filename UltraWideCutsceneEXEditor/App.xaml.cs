1164119dc5b1019bcfbc75d7986196d6e8f8ef49
Gareth <75081238+Gazz6767@users.noreply.github.com>
2024-07-09 22:05:01 +0100

    Add project files.

---
+++ UltraWideCutsceneEXEditor/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UltraWideCutsceneEXEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public ResourceDictionary ThemeDictionary
+        {
+            get { return Resources.MergedDictionaries[0]; }
+        }
+    }
+
+}
---
+++ UltraWideCutsceneEXEditor/ViewModels/MainwindowVM.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Controls;
+
+namespace UltraWideCutsceneEXEditor.ViewModels
+{
+    public partial class MainwindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        bool isPremade_Frame = false;
+
+        [ObservableProperty]
+        bool isCustomMade_Frame = false;
+
+        [ObservableProperty]
+        Page? selectedPage;
+
+        private Page premadePage = new Views.PremadePage();
+        private Page customMadePage = new Views.CustomPage();
+
+        [RelayCommand]
+        public void Load()
+        {
+            // find JSON file in Documents Folder and populate a service to store the data
+        }
+
+        partial void OnIsPremade_FrameChanged(bool value)
+        {
+            if (value) 
+            { 
+                IsPremade_Frame = true; 
+                IsCustomMade_Frame = false;
+                SelectedPage = premadePage;
+                return;
+            }
+            ClearPage();
+        }
+
+        partial void OnIsCustomMade_FrameChanged(bool value)
+        {
+            if (value) 
+            { 
+                IsCustomMade_Frame = true; 
+                IsPremade_Frame = false;
+                SelectedPage = customMadePage;
+                return;
+            }     
+            ClearPage();
+        }
+
+        private void ClearPage()
+        {
+            if (!IsCustomMade_Frame && !IsPremade_Frame)
+            {
+                SelectedPage = null;
+            }
+        }
+    }
+}
